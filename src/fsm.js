9bb4bdb4c2c5c8c4c71fcf8f15e1f55ae4ef1e96
Dmitrukk <d.dmitrukk@gmail.com>
2019-10-13 01:48:17 +0300

    Commit No.1

--- src/fsm.js
+++ src/fsm.js
@@ -1,61 +1,81 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    constructor(config) {
+      if (!config) throw new Error("config is missing");
+      this.initialState = config.initial;
+      this.states = config.states;
+      this.state = config.initial;
+      this.history = [];
+      this.buffer = [];
+    }
+  
+    getState() {
+      return this.state;
+    }
+  
+    changeState(state) {
+      if (this.states[state]) {
+        this.history.push(this.state);
+        this.state = state;
+        this.buffer = [];
+      } else {
+        throw new Error("state isn\'t exist")
+      }
+    }
+  
+    trigger(event) {
+      let newState = this.states[this.state].transitions[event];
+      if (newState) {
+        this.changeState(newState);
+      } else {
+        throw new Error("event in current state isn\'t exist");
+      }
+    }
+  
+    reset() {
+      this.changeState(this.initialState);
+    }
+  
+    getStates(event) {
+      if (!event) {
+        return Object.keys(this.states);
+      } else {
+        const result = [];
+        for (let key in this.states) {
+          if (this.states[key].transitions[event]) {
+            result.push(key);
+          }
+        } 
+        return result;
+      }
+    }
+  
+    undo() {
+      if (this.history.length) {
+        this.buffer.push(this.state);
+        this.state = this.history.pop();
+        return true;
+      } else {
+        return false;
+      }      
+    }
+  
+    redo() {
+      if (this.buffer.length) {
+        this.history.push(this.state);
+        this.state = this.buffer.pop();
+        return true;
+      } else {
+        return false;
+      }    
+    }
+  
+    clearHistory() {    
+      this.buffer = [];
+      this.history = [];
+    }
+  }
+  
+  module.exports = FSM;
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
-
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
-
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
-
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
-
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
-
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
-
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
-}
-
-module.exports = FSM;
-
-/** @Created by Uladzimir Halushka **/
+  /** @Created by Uladzimir Halushka **/
